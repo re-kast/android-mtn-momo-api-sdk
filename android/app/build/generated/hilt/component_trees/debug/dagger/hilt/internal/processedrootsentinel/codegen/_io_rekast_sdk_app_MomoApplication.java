23752f2835d14b2757287b514387c22066a1bf43
Benjamin Mwalimu <dubdabasoduba@gmail.com>
2026-04-16 14:58:58 +0300

    Feature/move to kmp (#91)
    * 🧪 Add test coverage
    
    * 💚 Make sure the Coverage and spotless are working
    
    * 🚀 Add the necessary badges for the CI
    
    * 💚 Make sure the github realease and package have change log
    
    * ✨ Move the project to kotlin multiplatform
    
    * 💚 Fix spotless and ci build. Clean up proeject
    
    * 🎨 Fix dokka workflow
    
    * 🛂 Remove unwanted files
    
    * 🙈 Add git ignore

--- android/app/build/generated/hilt/component_trees/debug/dagger/hilt/internal/processedrootsentinel/codegen/_io_rekast_sdk_app_MomoApplication.java
+++
@@ -1,9 +0,0 @@
-package dagger.hilt.internal.processedrootsentinel.codegen;
-
-import dagger.hilt.internal.processedrootsentinel.ProcessedRootSentinel;
-
-@ProcessedRootSentinel(
-    roots = "io.rekast.sdk.app.MomoApplication"
-)
-public final class _io_rekast_sdk_app_MomoApplication {
-}
